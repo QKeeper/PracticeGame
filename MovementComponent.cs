2b923230d9e481fec8aba0fa6c85ee8d39d201a4
QKeeper <59729070+QKeeper@users.noreply.github.com>
2023-05-05 16:29:44 +0500

    Add files via upload

---
+++ MovementComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using SharpDX.XInput;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monogame01
+{
+    public class MovementComponent : Component
+    {
+        public float speed = 0;
+        public Vector2 target = Vector2.Zero;
+    }
+}
