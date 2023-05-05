0285de56d84ee25cc874d8a3de6957a88acc0bfc
QKeeper <59729070+QKeeper@users.noreply.github.com>
2023-05-05 16:23:29 +0500

    Add files via upload

---
+++ classes/TransformComponent.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monogame01
+{
+    public class TransformComponent : Component
+    {
+        public Vector2 position = Vector2.Zero;
+        public Vector2 scale = Vector2.One;
+        public float rotation = 0;
+    }
+}
---
+++ classes/WeaponComponent.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Monogame01
+{
+    public class WeaponComponent : Component
+    {
+        public class Weapon
+        {
+            public Type type = Type.None;
+            public float Damage = 1f;
+            public float Penetration = 0f;
+            public float AttackRate = 1f;
+            public float MovementSpeed = 1f;
+
+            public enum Type
+            {
+                None,
+                Sword,
+                Spear,
+                Hammer
+            }
+        }
+
+        public Weapon Current;
+
+        public WeaponComponent(Weapon weapon)
+        {
+            if (weapon == null)
+                Current = new();
+            else
+                Current = weapon;
+        }
+
+
+    }
+}
